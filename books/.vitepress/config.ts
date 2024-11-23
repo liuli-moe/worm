d26705d3e844f4c6119523b960775e56830a6b73
rxliuli <rxliuli@gmail.com>
2024-11-23 12:49:08 +0800

    refactor: 调整目录结构，添加插图章节

--- books/.vitepress/config.ts
+++ books/.vitepress/config.ts
@@ -1,19 +1,83 @@
 import { defineConfig } from 'vitepress'
-import { generateSidebar } from 'vitepress-sidebar'
-import taskLists from '@hackmd/markdown-it-task-lists'
+import {
+  generateSidebar,
+  Sidebar,
+  SidebarItem,
+  SidebarMulti,
+} from 'vitepress-sidebar'
+import taskLists from '@rxliuli/markdown-it-task-lists'
 import { cjk } from 'markdown-it-cjk-space-clean'
+import { pagefindPlugin } from 'vitepress-plugin-pagefind'
+
+function aliasReadmeToIndex(sidebar: Sidebar): Sidebar {
+  if (Array.isArray(sidebar)) {
+    return sidebar
+      .filter((it) => it.link !== '/readme')
+      .map((it) => {
+        if (!it.items) {
+          return it
+        }
+        const findIndex = it.items?.findIndex(
+          (it) => !!it.link!.endsWith('/readme'),
+        )
+        if (findIndex === -1) {
+          return it
+        }
+        const del = it.items[findIndex]
+        return {
+          ...it,
+          link: del.link?.replace('readme', 'index'),
+          items: it.items.filter((it) => it !== del),
+        }
+      })
+  }
+  return Object.keys(sidebar).reduce((acc, key) => {
+    const it = sidebar[key]
+    acc[key] = {
+      ...it,
+      items: aliasReadmeToIndex(it.items) as SidebarItem[],
+    }
+    return acc
+  }, {} as SidebarMulti)
+}
 
 export default defineConfig({
+  rewrites: {
+    'readme.md': 'index.md',
+    ':a/readme.md': ':a/index.md',
+    ':a/:b/readme.md': ':a/:b/index.md',
+    ':a/:b/:c/readme.md': ':a/:b/:c/index.md',
+    ':a/:b/:c/:d/readme.md': ':a/:b/:c/:d/index.md',
+    ':a/:b/:c/:d/:e/readme.md': ':a/:b/:c/:d/:e/index.md',
+    ':a/:b/:c/:d/:e/:f/readme.md': ':a/:b/:c/:d/:e/:f/index.md',
+  },
   themeConfig: {
-    sidebar: generateSidebar({
-      documentRootPath: '/books',
-      useTitleFromFileHeading: true,
-    }),
+    sidebar: aliasReadmeToIndex(
+      generateSidebar({
+        documentRootPath: '/books',
+        useTitleFromFileHeading: true,
+        useTitleFromFrontmatter: true,
+        useFolderTitleFromIndexFile: true,
+        useFolderLinkFromIndexFile: true,
+      }),
+    ),
   },
   markdown: {
     config(md) {
       md.use(taskLists).use(cjk() as any)
     },
     breaks: true,
+  },
+  vite: {
+    plugins: [
+      pagefindPlugin({
+        forceLanguage: 'zh-cn',
+        btnPlaceholder: '搜索',
+        placeholder: '搜索文档',
+        emptyText: '空空如也',
+        heading: '共: {{searchResult}} 条结果',
+      }),
+    ],
+    publicDir: '../static',
   },
 })
--- books/readme.md
+++ books/readme.md
@@ -4,6 +4,6 @@
 > This is purely a Chinese translation fan project and I/we lay no claim to the ideas， characters， or story. The real author is J.C. McCrae， aka ‘Wildbow’， and the original version can be found at <http://www.parahumans.wordpress.com> .
 > 这纯粹是一个中文翻译的粉丝项目，我/我们对其中的想法、角色或故事不主张任何权利。真正的作者是 J.C. McCrae，又名 ‘Wildbow’，原版可以在 <http://www.parahumans.wordpress.com> 找到。
 
-![banner](../static/banner.png)
+![banner](/banner.png)
 
 主角是一个名叫泰勒・赫伯特的女高中生，是个失去母亲的黑发褐肤、性格阴郁的女孩。她在学校遭受霸凌，唯一支撑她活下去的希望是：不久前她经历了“触发事件”，获得了控制昆虫的超能力。如果她能顺利参加“监护者计划”，就能成为一个真正的超级英雄，为她那悲惨的人生增添一些意义...
